cc274ac7a0f060eceba096d8ceb3f60cca71797d
Cem <cem@example.com>
2026-02-07 19:17:35 +0300

    fix: remove next-pwa dependency - use manifest and install prompt instead

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
-
-const nextConfig = withPWA({
+const nextConfig = {
   reactStrictMode: true,
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  },
-})
+}
 
 module.exports = nextConfig
